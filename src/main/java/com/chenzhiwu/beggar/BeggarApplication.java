b6522e6f2f02a5310abb1f7e6b895e48e383eca3
zhiwuchen <zhiwu.chen@igg.com>
2019-12-11 17:54:41 +0800

    乞丐版商城

---
+++ src/main/java/com/chenzhiwu/beggar/BeggarApplication.java
@@ -0,0 +1,13 @@
+package com.chenzhiwu.beggar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeggarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeggarApplication.class, args);
+    }
+
+}
