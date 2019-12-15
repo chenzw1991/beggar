694a20fae0924c476b1c65d77205de200b2f620b
chenzw1991 <578346242@qq.com>
2019-12-15 23:13:35 +0800

    商城

--- src/main/java/com/chenzhiwu/beggar/BeggarApplication.java
+++ src/main/java/com/chenzhiwu/beggar/BeggarApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -13,6 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = "com.chenzhiwu.beggar.pojo")
 ////开启缓存注解
 @EnableCaching
+@EnableJpaAuditing // 使用jpa自动赋值
 public class BeggarApplication {
 
     public static void main(String[] args) {
